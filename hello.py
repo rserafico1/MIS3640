0c3cb5c180aaa79d0fc14a04772d4abf6e3fe6b7
rserafico1 <rserafico1@users.noreply.github.com>
2017-09-06 20:57:51 -0400

    Added class exercises

---
+++ hello.py
@@ -0,0 +1,35 @@
+print('hello, world!')
+
+print('Hey Jude', 'don\'t make it bad') 
+
+print ('The total number of overall medals in Rio 2016 is', 46 + 37 +38)
+
+print ('46 + 37 +38 =',46 + 37 +38)
+
+print('Please enter your name')
+
+name = input()
+print(name)
+
+message = 'I learned something cool!'
+
+n = 100
+pi = 3.14
+
+n = 100
+print(n)
+
+first_name = 'John'
+last_name = 'Lennon'
+print(first_name + last_name)
+
+print('Naah, na na nanana naah, nanana naah, hey Jude.' *10)
+
+print('Hello, %s' % first_name)
+
+print('Today is %2d-%02d.' % (9, 5))
+
+minute = 45 # current time
+
+# compute the percentage of the hour that has elapsed
+percent = (minute *100) /60 
