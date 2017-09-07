fa5a5c7d21005d4afdfb54896e81e17dad30ac1b
rserafico1 <rserafico1@users.noreply.github.com>
2017-09-06 20:57:29 -0400

    Answered exercise 1 questions

--- calc.py
+++ calc.py
@@ -1,3 +1,27 @@
 print(42*60+42)
 print(10*1.61)
 print(2562/16.1)
+
+#exercise 1 due 9/6 by 11:59pm
+pi = 3.14
+print((4/3)*pi*5**3) 
+print('The volume of a sphere with a radius of 5 units is', (4/3)*pi*5**3) # 1 Final Answer
+
+print(24.95*.4) # cover price * 40% discount = discount amount
+print(24.95-9.98) # cover price - discount amount = price per book
+print(14.97*60) # price per book (rounded up) * number of copies = total costs of books alone
+print(3+(.74*59)) # shipping cost for first book + (shipping cost for each additional book * remaining quantity) = total shipping cost
+print(898.2+46.66) # total cost of books alone + total shipping costs = total wholesale cost for 60 copies
+print('The total wholesale cost for 60 copies is $', 898.2+46.66) # 2 Final Answer
+
+print(((8*60)+15)) # ( [8] minutes * 60 seconds) + [15] seconds = total seconds for that first mile
+print(((21*60)+36)) # ( [7+7+7] minutes * 60 seconds) + [12+12+12] seconds = total seconds for those next 3 miles
+print((495+1296+495)/60) # (first mile seconds + next 3 miles seconds + final mile seconds [same length as first mile]) / 60 seconds = total minutes from running
+print(60-52) # minutes in a hour - time = remaining minutes of previous hour
+print(38.1-8) # total running minutes - remaining minutes of previous hour
+print('7:30 am') # 6:52am + 8 min = 7:00am + 30.1 min = 7:30am
+print('You will get home at 7:30am for breakfast') # 3 Final Answer
+
+print(89-82) # new number - original number = increase amount
+print(7/82) # increase amount / original = percentage of the increase
+print('The percentage of the increase is 8.5%') # 4 Final Answer
