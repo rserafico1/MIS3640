9e673a0494e45fe3a443a6903b8e2745334c5350
rserafico1 <rserafico1@users.noreply.github.com>
2017-09-08 21:04:11 -0400

    Class exercises

---
+++ type_demo.py
@@ -0,0 +1,18 @@
+import math
+print(math.pi)
+print(math.sqrt(85))
+
+import random 
+print(random.random())
+random.choice([1, 2, 3, 4])
+
+age = int(input("Please enter your age: "))
+
+if age > 21:
+    print('Yes, you can.')
+else:
+    print('No, you are not allowed.') 
+
+import time
+time.time()
+1437746094.5735958
